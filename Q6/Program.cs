8101fa0d58667e0e320d8a5c3d54ae0af01327e4
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:43:13 +0530

    Write a program to Calculate power of number using tail recursion in C

---
+++ Q6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+            int power = 4;
+
+            int result = CalculatePower(number, power);
+            Console.WriteLine($"{number} raised to the power of {power} is: {result}");
+        }
+
+        static int CalculatePower(int number, int power, int partialResult = 1)
+        {
+            if (power == 0)
+            {
+                return partialResult;
+            }
+
+            return CalculatePower(number, power - 1, partialResult * number);
+        }
+    }
+
+}
