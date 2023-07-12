181b3d6ee63f10475ad816aa3d3324e8a2149b75
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:51:55 +0530

    Given an array of integers, write a C# method to total all the values that are even numbers

---
+++ SportzInteractive/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportzInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sumOfEvens = SumEvenNumbers(numbers);
+            Console.WriteLine("Sum of even numbers: " + sumOfEvens);
+        }
+
+
+        public static int SumEvenNumbers(int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
