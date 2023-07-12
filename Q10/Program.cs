002311587a9e7c7a1b4af21426d055da9fa83d12
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:47:16 +0530

    In the player list below the “IsRetired” flag has been inversely (i.e. all 0s should be 1s and all 1s should be 0s) set for all players
    Write a program to update the “IsRetired” flag for all players,
    without using any loops.
    Player List:
    [
    {"PlayerName": "Virat Kholi", "IsRetired" : 1},
    {"PlayerName": "M.S. Dhoni", "IsRetired" : 1},
    {"PlayerName": "Hardik Pandya", "IsRetired" : 1},
    {"PlayerName": "Rohit Sharma", "IsRetired" : 1},
    {"PlayerName": "Sachin Tendulkar", "IsRetired" : 0},
    {"PlayerName": "Rahul Dravid", "IsRetired" : 0},
    {"PlayerName": "Sourav Ganguly", "IsRetired" : 0},
    {"PlayerName": "VVS Laxman", "IsRetired" : 0}
    ]

---
+++ Q10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>
+        {
+            new Player { PlayerName = "Virat Kholi", IsRetired = 1 },
+            new Player { PlayerName = "M.S. Dhoni", IsRetired = 1 },
+            new Player { PlayerName = "Hardik Pandya", IsRetired = 1 },
+            new Player { PlayerName = "Rohit Sharma", IsRetired = 1 },
+            new Player { PlayerName = "Sachin Tendulkar", IsRetired = 0 },
+            new Player { PlayerName = "Rahul Dravid", IsRetired = 0 },
+            new Player { PlayerName = "Sourav Ganguly", IsRetired = 0 },
+            new Player { PlayerName = "VVS Laxman", IsRetired = 0 }
+        };
+
+            List<Player> updatedPlayers = players.Select(p => new Player { PlayerName = p.PlayerName, IsRetired = (p.IsRetired == 0) ? 1 : 0 }).ToList();
+
+            Console.WriteLine("Updated Player List:");
+            foreach (var player in updatedPlayers)
+            {
+                Console.WriteLine("Player Name: " + player.PlayerName + ", IsRetired: " + player.IsRetired);
+            }
+        }
+    }
+
+    class Player
+    {
+        public string PlayerName { get; set; }
+        public int IsRetired { get; set; }
+    }
+
+}
