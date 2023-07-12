647df50eb270ebee7c46b38d721411277e3cba80
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:44:47 +0530

    Write an over loaded function in c# to calculate an area of a square or a rectangle.

---
+++ Q8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareArea = CalculateArea(5);
+            Console.WriteLine("Area of square: " + squareArea);
+
+            double rectangleArea = CalculateArea(4, 6);
+            Console.WriteLine("Area of rectangle: " + rectangleArea);
+        }
+
+        static double CalculateArea(double sideLength)
+        {
+            // Calculate area of square
+            double area = sideLength * sideLength;
+            return area;
+        }
+
+        static double CalculateArea(double length, double width)
+        {
+            // Calculate area of rectangle
+            double area = length * width;
+            return area;
+        }
+    }
+
+}
