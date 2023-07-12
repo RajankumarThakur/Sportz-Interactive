4a3e8aea6e54d02037de771ecc93c03f077e603d
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:36:25 +0530

    Write a program to swap two numbers without using a temp variable in C#

---
+++ Q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine("Before swapping:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            // Swap the numbers without a temporary variable
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+
+            Console.WriteLine("After swapping:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+
+}
