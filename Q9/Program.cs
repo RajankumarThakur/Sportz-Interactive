f0f65f392debd81d1fe00eccb2233f2d189df5ea
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:46:32 +0530

    If Batting Momentum = Runs Scored * Strike Rate, write a program that will return the batsman with
    the best batting momentum from the below object.
    [
    {"Batsman": "Virat Kholi", "RunsScored": 50, "StrikeRate": 78.30},
    {"Batsman": "M.S.Dhoni", "RunsScored": 61, "StrikeRate": 58.90},
    {"Batsman": "Rohit Sharma", "RunsScored": 13, "StrikeRate": 124.0}
    ]

---
+++ Q9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q9
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BatsmanData> batsmen = new List<BatsmanData>
+        {
+            new BatsmanData { Batsman = "Virat Kholi", RunsScored = 50, StrikeRate = 78.30 },
+            new BatsmanData { Batsman = "M.S.Dhoni", RunsScored = 61, StrikeRate = 58.90 },
+            new BatsmanData { Batsman = "Rohit Sharma", RunsScored = 13, StrikeRate = 124.0 }
+        };
+
+            string bestBatsman = GetBatsmanWithBestMomentum(batsmen);
+            Console.WriteLine("Batsman with the best batting momentum: " + bestBatsman);
+        }
+
+        static string GetBatsmanWithBestMomentum(List<BatsmanData> batsmen)
+        {
+            string bestBatsman = "";
+            double highestMomentum = 0;
+
+            foreach (var batsman in batsmen)
+            {
+                double momentum = batsman.RunsScored * batsman.StrikeRate;
+
+                if (momentum > highestMomentum)
+                {
+                    highestMomentum = momentum;
+                    bestBatsman = batsman.Batsman;
+                }
+            }
+
+            return bestBatsman;
+        }
+    }
+
+    class BatsmanData
+    {
+        public string Batsman { get; set; }
+        public int RunsScored { get; set; }
+        public double StrikeRate { get; set; }
+    }
+
+}
