06b5a9a194ad17ffa70cbc546508a3681fef0081
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:44:00 +0530

    Write a program to iterate through Enum values in C#?
     public enum Colors  { red, blue, green, yellow  }

---
+++ Q7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    public enum Colors
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Colors color in Enum.GetValues(typeof(Colors)))
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+
+}
