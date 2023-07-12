416110134b0eec126e118b857f1f0bd36ea52ddc
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:50:19 +0530

    Create a C# object which returns the following JSON result when serialized.
    {
    "Name_Full": "Sunrisers Hyderabad",
    "Name_Short": "SRH",
    "Players":
    {
    "5380": {"Position": "1", "Name_Full": "David Warner", "IsCaptain": true},
    "3722": {"Position": "2", "Name_Full": "Shikhar Dhawan", "IsCaptain": false}
    }
    }

---
+++ Q11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Q11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team
+            {
+                Name_Full = "Sunrisers Hyderabad",
+                Name_Short = "SRH",
+                Players = new Players
+            {
+                { "5380", new Player { Position = "1", Name_Full = "David Warner", IsCaptain = true } },
+                { "3722", new Player { Position = "2", Name_Full = "Shikhar Dhawan", IsCaptain = false } }
+            }
+            };
+
+            string json = JsonConvert.SerializeObject(team, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+
+    class Team
+    {
+        [JsonProperty("Name_Full")]
+        public string Name_Full { get; set; }
+
+        [JsonProperty("Name_Short")]
+        public string Name_Short { get; set; }
+
+        [JsonProperty("Players")]
+        public Players Players { get; set; }
+    }
+
+    class Players : Dictionary<string, Player> { }
+
+    class Player
+    {
+        [JsonProperty("Position")]
+        public string Position { get; set; }
+
+        [JsonProperty("Name_Full")]
+        public string Name_Full { get; set; }
+
+        [JsonProperty("IsCaptain")]
+        public bool IsCaptain { get; set; }
+    }
+
+}
