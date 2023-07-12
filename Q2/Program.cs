5c4f15f533fab5e24e5ee740e073e8e795637784
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:34:12 +0530

    Add files via upload

---
+++ Q2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("valar morghulis");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("valar");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("morghulis");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+
+}
