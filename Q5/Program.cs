17795ea4020fec7fc773a5aefa168c7714136ce7
RajankumarThakur <97182200+RajankumarThakur@users.noreply.github.com>
2023-07-12 10:40:56 +0530

    Write code to calculate the circumference of the circle, without modifying the Circle class itself. ]
    Given an instance circle of the following class:
    public sealed class Circle
    {
    private double radius;
    public double Calculate (Func<double, double> op)
    {
    return op(radius);
    }
    }
    Write code to calculate the circumference of the circle, without modifying the Circle class itself.
    Hint: Use [function-valued parameters]

---
+++ Q5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+
+    public sealed class Circle
+    {
+        private double radius;
+
+        public double Calculate(Func<double, double> op)
+        {
+            return op(radius);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.Calculate((r) => 2 * Math.PI * r);
+            
+        }
+    }
+
+}
